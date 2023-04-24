b911fcb51162f14db930371f6c75c7b8477faf30
Gavin C <59036563+Gavin-TC@users.noreply.github.com>
2023-04-24 18:25:40 -0400

    Initial commit

---
+++ spacegame/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Spacegame
+{
+    class Game
+    {
+        private bool isRunning;
+        
+        public void Initialize()
+        {
+            // Perform any game initialization tasks here
+            // such as setting up game objects, loading assets, etc.
+            
+            
+            isRunning = true;  // Set the game to running state
+        }
+        
+        public void Update()
+        {
+            // Update game state, handle input, etc.
+        }
+        
+        public void Render()
+        {
+            // Render the game screen, draw ASCII art, etc.
+        }
+
+        public void Run()
+        {
+            Initialize();
+
+            while (isRunning)
+            {
+                Update();
+                Render();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var game = new Game();
+            game.Run();
+        }
+    }
+}
