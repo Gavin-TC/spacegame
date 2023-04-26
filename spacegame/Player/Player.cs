f4a9be0eb8ff7641e9853448a5ea299b85d0464a
Gavin C <59036563+Gavin-TC@users.noreply.github.com>
2023-04-25 21:02:12 -0400

    initial variables for the player

--- spacegame/Player/Player.cs
+++ spacegame/Player/Player.cs
@@ -2,5 +2,21 @@
 
 public class Player
 {
-    
+    private int px; // Player x and y
+    private int py;
+    private char playerChar;
+
+    public int health = 100;
+    public int exp = 0; // Experience points
+    public int level = 0; // Current level
+
+    public void Movement()
+    {
+        
+    }
+
+    public void Update()
+    {
+        
+    }
 }
