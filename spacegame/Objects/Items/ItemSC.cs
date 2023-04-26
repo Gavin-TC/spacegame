c1800412d76a8119b0013599247096f4f3b84efa
Gavin C <59036563+Gavin-TC@users.noreply.github.com>
2023-04-25 21:02:20 -0400

    adding item stuff

---
+++ spacegame/Objects/Items/ItemSC.cs
@@ -0,0 +1,13 @@
+﻿namespace Spacegame.Objects.Items;
+
+public class ItemSC
+{
+    public string name;
+    public int weight;
+
+    public ItemSC(string name, int weight)
+    {
+        this.name = name;
+        this.weight = weight;
+    }
+}
