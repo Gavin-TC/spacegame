7f7307e0ebf62a2cb89d0826ce13e61648a1d88e
Gavin C <59036563+Gavin-TC@users.noreply.github.com>
2023-04-26 01:39:38 -0400

    renaming

---
+++ spacegame/Player/PlayerClass.cs
@@ -0,0 +1,33 @@
+﻿using Spacegame.Input;
+
+namespace Spacegame.Player
+{
+    public class PlayerClass
+    {
+        static DetectInput inputDetector = new DetectInput();
+        Thread inputThread = new Thread(inputDetector.CheckInput);
+
+        private int px; // Player x and y
+        private int py;
+        private char playerChar;
+
+        public int health { get; set; }
+        public int exp { get; set; } // Experience points
+        public int level { get; set; } // Current level
+
+        public void Initialize()
+        {
+            inputThread.Start();
+        }
+
+        public void Movement()
+        {
+            Console.WriteLine();
+        }
+
+        public void Update()
+        {
+            Movement(); // Check if player has clicked a button
+        }
+    }
+}
