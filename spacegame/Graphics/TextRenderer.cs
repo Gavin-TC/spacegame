836abcb4e0160a7cc4406efae84c0d61764f3f19
Gavin C <59036563+Gavin-TC@users.noreply.github.com>
2023-04-27 10:52:05 -0400

    fixed TextRenderer flickering

--- spacegame/Graphics/TextRenderer.cs
+++ spacegame/Graphics/TextRenderer.cs
@@ -24,21 +24,14 @@ public class TextRenderer
         // Sets console cursor the top left, so when next write happens it will overwrite
         // what was already there instead of clearing it first
         // </summary>
-        Console.SetCursorPosition(0, 16);
-
-        for (var rows = 0; rows < screenHeight; rows++)
-        {
-            for (var cols = 0; cols < screenWidth; cols++)
-            {
-                characterBuffer[rows, cols] = ' ';
-                Console.Write(characterBuffer[rows, cols]);
-            }
-        }
+        Console.SetCursorPosition(0, screenHeight+10); // screenHeight is initialized 10 'pixels' more than MapRenderer.screenHeight;
     }
 
     public void DrawText(int x, int y, string text)
     {
         Console.SetCursorPosition(x, y);
-        Console.WriteLine(text);
+        
+        // Janky way to do this, but the extra space clears any left over zeroes when subtracting a number...
+        Console.WriteLine(text + " "); 
     }
 }
