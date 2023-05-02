9ececaf24473ccec3733916c5e1579edd6033821
Gavin C <59036563+Gavin-TC@users.noreply.github.com>
2023-05-02 02:10:07 -0400

    Update PlayerClass.cs

--- spacegame/Player/PlayerClass.cs
+++ spacegame/Player/PlayerClass.cs
@@ -1,32 +1,38 @@
-﻿using Spacegame.Input;
+﻿using System.Globalization;
+using Spacegame.Input;
+using Spacegame.Utilities;
 
 namespace Spacegame.Player
 {
     public class PlayerClass
     {
-        private DetectInput _detectInput;
+        private HandleInput? _handleInput;
 
-        public int px; // Player x and y
+        public int px; // Player left and right coordinate
         public int py;
+
+        // public int dirX; // Player direction up and down
+        // public int dirY;
+
+        public bool goingRight;
+        public bool goingUp;
+        
         public char playerChar;
-
-        private bool up;
-        private bool down;
-        private bool right;
-        private bool left;
-
+        public string playerName;
+        
         public int speed { get; set; }
 
         public int health { get; set; }
         public int exp { get; set; } // Experience points
         public int level { get; set; } // Current level
 
-        public PlayerClass(int speed, int health, int exp, int level)
+        public PlayerClass(string playerName, int speed, int health, int exp, int level)
         {
             this.speed = speed;
             this.health = health;
             this.exp = exp;
             this.level = level;
+            this.playerName = playerName;
         }
         
         public void Initialize()
@@ -34,10 +40,10 @@ namespace Spacegame.Player
             px = 5;
             py = 5;
 
-            _detectInput = new DetectInput();
-            _detectInput.InputEvent += OnInput;
+            _handleInput = new HandleInput();
+            _handleInput.InputEvent += OnInput;
 
-            Thread inputThread = new Thread(_detectInput.CheckInput);
+            var inputThread = new Thread(_handleInput.CheckInput);
             inputThread.Start();
         }
 
@@ -47,30 +53,91 @@ namespace Spacegame.Player
             switch (key)
             {
                 case ConsoleKey.W:
-                    py -= 1;
+                    goingUp = true;
+                    if (!CheckWall(true))
+                    {
+                        py -= 1;
+                    }
                     break;
+                
                 case ConsoleKey.S:
-                    py += 1;
+                    goingUp = false;
+                    if (!CheckWall(true))
+                    {
+                        py += 1;
+                    }
                     break;
+                
                 case ConsoleKey.D:
-                    px += 1;
+                    goingRight = true;
+                    if (!CheckWall(false))
+                    {
+                        px += 1;
+                    }
                     break;
+                
                 case ConsoleKey.A:
-                    px -= 1;
+                    goingRight = false;
+                    if (!CheckWall(false))
+                    {
+                        px -= 1;
+                    }
+                    break;
+                
+                case ConsoleKey.E: // interact;
                     break;
             }
-            
-            // This doesn't work, continues to read input and delays it, so you keep moving if you've held the key.
+
+
+            // This doesn't work, continues to read input and delays it, so you keep moving becuase you've held the key.
             //Thread.Sleep(speed);
         }
-
-        public void Movement()
+        
+        public bool CheckWall(bool checkUp)
         {
+            if (checkUp)
+            {
+                if (goingUp && Global.currentMap[py - 1, px] == Global.wallChar
+                    || !goingUp && Global.currentMap[py + 1, px] == Global.wallChar)
+                {
+                    return true;
+                }
+            }
+
+            if (!checkUp)
+            {
+                if (goingRight && Global.currentMap[py, px + 1] == Global.wallChar
+                    || !goingRight && Global.currentMap[py, px - 1] == Global.wallChar)
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
+        public char Above()
+        {
+            return Global.currentMap[px, py - 1];
+        }
+
+        public char Below()
+        {
+            return Global.currentMap[px, py + 1];
+        }
+
+        public char Right()
+        {
+            return Global.currentMap[px + 1, py];
+        }
+
+        public char Left()
+        {
+            return Global.currentMap[px - 1, py];
+        }
+        
         public void Update(double deltaTime)
         {
-            Movement();
         }
     }
 }
