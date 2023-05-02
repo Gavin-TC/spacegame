648e98f4eef711d1684d86eec54b664ace438547
Gavin C <59036563+Gavin-TC@users.noreply.github.com>
2023-05-02 02:09:59 -0400

    renamed

---
+++ spacegame/Input/HandleInput.cs
@@ -0,0 +1,21 @@
+﻿using static Spacegame.Game;
+
+namespace Spacegame.Input
+{
+    public class HandleInput
+    {
+        public delegate void InputEventHandler(ConsoleKey key);
+        public event InputEventHandler InputEvent;
+        
+        public void CheckInput()
+        {
+            while (true)
+            {
+                ConsoleKey key = Console.ReadKey(true).Key;
+                InputEvent?.Invoke(key);
+            }
+        }
+
+        
+    }
+}
