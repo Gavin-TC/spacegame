6432e7cb93fc9099c64ca0547fcb9ed382a134d0
Gavin C <59036563+Gavin-TC@users.noreply.github.com>
2023-05-01 20:17:17 -0400

    Created Keybinds.cs (NOT USED)

---
+++ spacegame/Input/Keybinds.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Spacegame.Input;
+
+// <summary>
+// TODO: MAKE THIS FILE ACTUALLY USEFUL AND CHANGE THE CODE IN PLAYERCLASS.CS
+// </summary>
+
+public class Keybinds
+{
+    
+    public void SetKeybinds()
+    {
+        // todo: set the keybindings value to a getter so that the user can change controls
+        
+        Dictionary<string, char> Keybindings = new Dictionary<string, char>();
+
+        Keybindings.Add("UP", 's');
+        Keybindings.Add("DOWN", 's');
+        Keybindings.Add("LEFT", 'a');
+        Keybindings.Add("RIGHT", 'd');
+        Keybindings.Add("INTERACT", 'e');
+        Keybindings.Add("INVENTORY", 'i');
+    }
+}
