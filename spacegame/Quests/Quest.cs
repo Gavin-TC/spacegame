49806085c02468f356036dec2f9273cd9234e104
Gavin C <59036563+Gavin-TC@users.noreply.github.com>
2023-05-02 22:42:35 -0400

    created quest class and a questmanager

---
+++ spacegame/Quests/Quest.cs
@@ -0,0 +1,10 @@
+﻿namespace Spacegame.Quests;
+
+public class Quest
+{
+    public string questName { get; set; }
+    public string questDescription { get; set; }
+    public bool isCompleted { get; set; }
+    public int experienceReward { get; set; }
+    public int creditReward { get; set; }
+}
---
+++ spacegame/Quests/QuestManager.cs
@@ -0,0 +1,30 @@
+﻿namespace Spacegame.Quests;
+
+public class QuestManager
+{
+    private List<Quest> _activeQuests = new List<Quest>();
+    private List<Quest> _completedQuests = new List<Quest>();
+
+    public void AddQuest(Quest quest)
+    {
+        _activeQuests.Add(quest);
+    }
+
+    public void CompleteQuest(Quest quest)
+    {
+        _activeQuests.Remove(quest);
+        _completedQuests.Add(quest);
+        
+        // Add rewards below
+    }
+
+    public IEnumerable<Quest> GetActiveQuests()
+    {
+        return _activeQuests;
+    }
+
+    public IEnumerable<Quest> GetCompletedQuests()
+    {
+        return _completedQuests;
+    }
+}
