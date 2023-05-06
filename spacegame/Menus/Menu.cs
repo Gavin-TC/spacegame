206ea2383a0dfac62f9bf25b0236a1c30d83bdd8
Gavin C <59036563+Gavin-TC@users.noreply.github.com>
2023-05-06 03:25:44 -0400

    Create Menu.cs

---
+++ spacegame/Menus/Menu.cs
@@ -0,0 +1,57 @@
+﻿using Spacegame.Graphics;
+using Spacegame.Utilities;
+
+namespace Spacegame.Menus;
+
+public class Menu
+{
+    private readonly Game game;
+    
+    public Menu(Game game)
+    {
+        this.game = game;
+    }
+    
+    public void ShowMenu()
+    {
+        var textRenderer = new TextRenderer(30, 20);
+        
+        Console.Clear();
+        
+        Console.WriteLine(" PAUSED ");
+        Console.WriteLine();
+        Console.WriteLine("Press enter to resume");
+        Console.WriteLine("1) Options");
+        Console.WriteLine("2) Help");
+        Console.WriteLine("3) Main Menu");
+        Console.WriteLine();
+        int choice = Console.Read();
+
+        switch (choice)
+        {
+            case 1:
+                Global.menuActive = false;
+                game.Run();
+                break;
+            
+            case 2:
+                break;
+            
+            case 3:
+                break;
+            
+            default:
+                Global.menuActive = false;
+                game.Run();
+                break;
+        }
+    }
+    
+    private class HelpMenu
+    {
+    }
+
+    private class MainMenu
+    {
+    }
+}
