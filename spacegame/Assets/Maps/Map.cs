bccf124315a69c160adb976393db477c11f18ea3
Gavin C <59036563+Gavin-TC@users.noreply.github.com>
2023-05-06 03:24:22 -0400

    Create Map.cs

---
+++ spacegame/Assets/Maps/Map.cs
@@ -0,0 +1,13 @@
+﻿namespace Spacegame.Assets.Maps;
+
+public class Map
+{
+    public string name;
+    public List<List<int>> tiles;
+
+    public Map(string name, List<List<int>> tiles)
+    {
+        this.name = name;
+        this.tiles = tiles;
+    }
+}
